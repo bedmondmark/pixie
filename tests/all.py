544b91cbdc2d04299d90d6ce1460767c1726c7a9
Mark Smith <mark.smith@rankinteractive.com>
2013-06-11 20:15:52 +0100

    Add a bunch of files that will be required

---
+++ tests/all.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import
+import unittest
+
+from layout import *
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/layout.py
@@ -0,0 +1,8 @@
+import unittest
+
+class DefaultLayoutTestCase(unittest.TestCase):
+    def test_layout(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
