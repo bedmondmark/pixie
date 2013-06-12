330189aa3ded0c1fcc65890f11fcf00b89ed3ad7
Mark Smith <mark.smith@rankinteractive.com>
2013-06-12 10:27:51 +0100

    One purposefully failing test

--- tests/all.py
+++ tests/all.py
@@ -7,4 +7,5 @@ import unittest
 from layout import *
 
 if __name__ == '__main__':
+    sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
     unittest.main()
--- tests/layout.py
+++ tests/layout.py
@@ -1,8 +1,19 @@
+import os.path
+import sys
 import unittest
 
+
 class DefaultLayoutTestCase(unittest.TestCase):
+    @property
+    def _layout(self):
+        import pixie.layout
+        return pixie.layout
+
     def test_layout(self):
-        pass
+        layout = self._layout
+        self.fail("This test is not finished yet!")
+
 
 if __name__ == '__main__':
+    sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
     unittest.main()
