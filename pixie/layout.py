bfcc665f922e25d6bc7c73720f8f1d66970b5c34
Mark Smith <mark.smith@rankinteractive.com>
2013-06-12 10:28:49 +0100

    Add a bunch of sprite sorting functions

--- pixie/layout.py
+++ pixie/layout.py
@@ -1,2 +1,26 @@
 # -*- coding: utf-8 -*-
 
+
+class Sprite(object):
+    def __init__(name, width, height):
+        self.width = width
+        self.height = height
+        self.name = name
+
+
+# Sorting key functions -------------------------------------------------------
+
+def sort_width(r):
+    return r.width
+
+def sort_height(r):
+    return r.height
+
+def sort_name(r):
+    return r.name
+
+def sort_area(r):
+    return r.width * r.height
+
+def sort_circumference(r):
+    return r.width + r.height
