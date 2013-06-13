194293f386d09e47fa416968b1bb497f23005638
Mark Smith <mark.smith@rankinteractive.com>
2013-06-13 12:13:04 +0100

    Add first stab at linear layout algorithms

--- pixie/layout.py
+++ pixie/layout.py
@@ -2,10 +2,31 @@
 
 
 class Sprite(object):
-    def __init__(name, width, height):
+    def __init__(self, name, width, height):
         self.width = width
         self.height = height
         self.name = name
+
+    @property
+    def area(self):
+        return self.width * self.height
+
+    def __repr__(self):
+        return "Sprite({name}, {width}, {height})".format(**self.__dict__)
+
+
+class SpritePosition(object):
+    def __init__(self, sprite, x, y):
+        self.sprite = sprite
+        self.x = x
+        self.y = y
+
+
+class Layout(object):
+    def __init__(self):
+        self.width = 0
+        self.height = 0
+        self.sprite_positions = []
 
 
 # Sorting key functions -------------------------------------------------------
@@ -13,14 +34,40 @@ class Sprite(object):
 def sort_width(r):
     return r.width
 
+
 def sort_height(r):
     return r.height
+
 
 def sort_name(r):
     return r.name
 
+
 def sort_area(r):
     return r.width * r.height
 
+
 def sort_circumference(r):
     return r.width + r.height
+
+
+def sort_sprites(sprites, sort_name):
+    sort_func = globals()['sort_'+sort_name]
+    return sorted(sprites, key=sort_func, reverse=sort_name != 'name')
+
+
+def layout_horizontal(sprites):
+    result = Layout()
+    for sprite in sprites:
+        result.sprite_positions.append(SpritePosition(sprite, result.width, 0))
+        result.width += sprite.width
+    return result
+
+
+def layout_vertical(sprites):
+    result = Layout()
+    for sprite in sprites:
+        result.sprite_positions.append(SpritePosition(sprite, 0, result.height))
+        result.height += sprite.height
+    return result
+
