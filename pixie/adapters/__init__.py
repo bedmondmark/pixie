21ded90caddc04531bba79455ed757ae8e65b702
Mark Smith <mark.smith@practicalpoetry.co.uk>
2013-06-13 22:03:39 +0100

    Improve pillow trim demo

--- pixie/adapters/__init__.py
+++ pixie/adapters/__init__.py
@@ -5,15 +5,15 @@ from __future__ import absolute_import
 
 def trim_demo():
     "Temporary function - just an experiment."
-    from PIL import Image as image
+    from PIL import Image as image, ImageChops as imagechops
 
     # Test how image trimming will work:
-    im = image.open('tests/data/test.png')
-    print im.getbbox()
-    im = im.crop(im.getbbox())
-    im.show()
+    im = image.open('tests/data/Slice 1.png')
+    print im.mode, im.size, im.getbbox()
+    crop_area = im.split()[3].getbbox()
+    return im.crop(crop_area), crop_area
 
 
 # Temporary code:
 if __name__ == '__main__':
-    trim_demo()
+    print trim_demo()
