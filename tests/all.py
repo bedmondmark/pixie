03a09b66287a75e6812c8fe188c0a825821e7d32
Mark Smith <mark.smith@practicalpoetry.co.uk>
2013-06-13 22:29:49 +0100

    Make tests/all.py work again

--- tests/all.py
+++ tests/all.py
@@ -2,11 +2,15 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import absolute_import
-import unittest
 
-from layout import *
-from pillow_adapter import *
+import os.path
+import sys
+import unittest
 
 if __name__ == '__main__':
     sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+    from layout import *
+    from pillow_adapter import *
+
     unittest.main()
