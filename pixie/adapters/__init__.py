e415502d64137d21b67acd6d6e6cedecd8a4215c
Mark Smith <mark.smith@rankinteractive.com>
2013-06-13 17:31:16 +0100

    Sample code for image trimming

--- pixie/adapters/__init__.py
+++ pixie/adapters/__init__.py
@@ -1,3 +1,17 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import absolute_import
+
+def trim_demo():
+    "Temporary function - just an experiment."
+    from PIL import Image as image
+
+    # Test how image trimming will work:
+    im = image.open('tests/data/test.png')
+    print im.getbbox()
+    im = im.crop(im.getbbox())
+    im.show()
+
+# Temporary code:
+if __name__ == '__main__':
+    trim_demo()
