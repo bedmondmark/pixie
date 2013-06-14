9fc1004aa1139636fc11c962b7f6c91128d5bb69
Mark Smith <mark.smith@rankinteractive.com>
2013-06-14 18:39:04 +0100

    Improve runner test

--- tests/test_runner.py
+++ tests/test_runner.py
@@ -12,7 +12,7 @@ class RunnerTestCase(unittest.TestCase):
     def test_parse_args(self):
         runner = self._runner
         options = runner.parse_args(['an_image.png'])
-
+        self.assertEqual(['an_image.png'], options.images)
 
 if __name__ == '__main__':
     sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
